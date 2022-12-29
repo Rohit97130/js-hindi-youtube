660006e488483b874107803e0bc0de8f28344b0a
Rohit jain <114089911+Rohit97130@users.noreply.github.com>
2022-12-29 20:28:32 +0000

    datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 14453
+let accountEmail = "hitesh@google.com"
+var accountName = "1234"
+accountCity = "jablapur"
+let accountState;
+
+/*
+prefer not to use var beacuse of issue of scope
+*/
+
+// accountId = 24 not allowed
+accountEmail= "jj j f"
+accountName = "666555"
+accoutCity= "BANGLORE"
+console.log(accountId);
+console.table([accountId,accountName, accountEmail,accountState,accountCity]);
+
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all Js code as per newer version
+//  alert("hello") we can not  run because we are using not chrome
+
+console.log(3+3); // code readbility should be high
+
+
+let name ="hitesh"
+let age = 18
+let isLoggedIn = false
+// number =2 to power 53
+// bigint
+// string =>""
+// boolean => true /fale 
+// null => standalonre value // null is a objec 
+// undefined 
+// symbol => unique
+
+
+
+// object 
+console.log(typeof null); // object 
+console.log(typeof undefined ); // undefine
