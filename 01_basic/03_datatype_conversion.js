5db1e89d54a7f39a856aee41236fa79ed1d60e41
Rohit jain <114089911+Rohit97130@users.noreply.github.com>
2022-12-30 18:05:24 +0000

    good

---
+++ 01_basic/03_datatype_conversion.js
@@ -0,0 +1,28 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumbeer= Number(score);
+//console.log(typeof valueInNumbeer );
+//console.log(valueInNumbeer);
+
+//"33"=> 33
+// "33abc"=> nan
+//  true => 1 ; false=> 0
+
+
+let isLoggedIn =" h"
+
+let booleanIsLogged = Boolean(isLoggedIn);
+console.log(typeof booleanIsLogged);
+console.log(booleanIsLogged);
+
+//1 =true; 0>= false 
+// ""=> false
+//"hitesh"=> true
+let SomeNumber= 33
+
+let stringNumber = String(SomeNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
