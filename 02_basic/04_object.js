e4db36bb55990750d6f98718d7e539504085237c
Rohit jain <114089911+Rohit97130@users.noreply.github.com>
2023-12-08 18:38:36 +0000

    done

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -6,4 +6,54 @@ tinderUser.id ="123abc"
 tinderUser.name ="Sammy"
 tinderUser.isLogged=false
 
+
+// console.log(tinderUser);
+
+const regularUser ={
+    email:"some@gmail.com",
+    fullname: {
+       userfullname:{
+           firstname:"rohit",
+           lastname:"jain"
+       }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+
+
+const obj1= {1:"a", 2:"b"}
+const obj2= {3:"a", 4:"b"}
+// const obj3 = {obj1,obj2}
+const obj3 = Object.assign({},obj1,obj2)
+
+// const obj3 ={...obj1,...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id:1,
+        email:"rohitjain@gmail.com"
+    },
+    {
+        id:1,
+        email:"rohitjain"
+    },
+    {
+        id:1,
+        email:"rohitjain"
+    }
+  
+]
+// console.log(users[1].email);
+
+
 console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogged'));      //true
+
+
